687a4f429820512c4bebac9b60e5ca0896accd4b
PatrickPek <pekp@umich.edu>
2021-11-11 14:16:04 -0500

    alert dialogue for next button

--- app/src/main/java/com/example/surveyapp/VideoActivity.java
+++ app/src/main/java/com/example/surveyapp/VideoActivity.java
@@ -57,12 +57,13 @@ public class VideoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
-        //setting video view
-        setContentView(R.layout.videotask);
-        VideoView videoView = findViewById(R.id.videoView);
-        videoView.setVideoPath("android.resource://"+getPackageName()+"/"+R.raw.cat);
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);  //casting to VideoView is not Strictly required above API level 26
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.cat); //set the path of the video that we need to use in our VideoView
+        videoView.start();  //start() method of the VideoView class will start the video to play
         MediaController mediaController = new MediaController(this);
+        //link mediaController to videoView
         mediaController.setAnchorView(videoView);
+        //allow mediaController to control our videoView
         videoView.setMediaController(mediaController);
 
 
